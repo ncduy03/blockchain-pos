2f411d10a35d6b6e197057723aac6ddf79ef3c14
_ncduy_ <ncduy1906@gmail.com>
2023-12-02 16:51:57 +0700

    deleted:    AccountModel.py 	deleted:    Block.py 	deleted:    Blockchain.py 	deleted:    BlockchainUtils.py 	deleted:    Interaction.py 	deleted:    Lot.py 	deleted:    Main.py 	deleted:    Message.py 	deleted:    Node.py 	deleted:    NodeAPI.py 	deleted:    PeerDiscoveryHandler.py 	deleted:    ProofOfStake.py 	deleted:    README.md 	deleted:    SocketCommunication.py 	deleted:    SocketConnector.py 	deleted:    Test.py 	deleted:    Transaction.py 	deleted:    TransactionPool.py 	deleted:    Wallet.py

--- Message.py
+++
@@ -1,5 +0,0 @@
-class Message():
-    def __init__(self, senderConnector, messageType, data):
-        self.senderConnector = senderConnector
-        self.messageType = messageType
-        self.data = data
